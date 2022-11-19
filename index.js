761b766966f5a216366770edca6af82b001a273b
Richard Young <richardyoung@Richards-MacBook-Air.local>
2022-11-19 15:48:07 -0500

    in bed

--- index.js
+++ index.js
@@ -24,13 +24,35 @@ const Song = dbConnection.define(`song`, {
 const myAsyncFn = async () => {
     await Song.sync() // sync --> either CONNECTS or CREATES the table the model represents
 
+    //another way to create a new row!!
+    // const newSong = new Song({
+    //     title: "Hello",
+    //     duration: 7*60,
+    //     lyrics: "Jazma knows these"
+    // });
+
+    // await newSong.save();
+
+    // another way to create a new row!!! this uses .create(), which is new and save combined.
+    const newSong = Song.create({
+        title: "Hotel California",
+        duration: 4.5 * 60,
+        lyrics: "Welcome to the hotel california"
+    });
+
     // you can add WHERE clauses
-    const songs = await Song.findAll();
+    const songs = await Song.findAll({
+        where: {
+            title: "Yesterday"
+        }
+    });
 
     // console.log(songs);
     // console.log(songs[0]);
     console.log("songs[0].title value is:",songs[0].title);
     // console.log(songs[1]);
+
+
 };
 
 myAsyncFn();

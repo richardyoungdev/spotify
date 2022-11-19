17a865ad45d71bbc60505b05f01dd4e5ca9edc5e
Richard Young <richardyoung@Richards-MacBook-Air.local>
2022-11-19 17:44:19 -0500

    richie artist

--- index.js
+++ index.js
@@ -21,10 +21,62 @@ const Song = dbConnection.define(`song`, {
     }
 });
 
-const myAsyncFn = async () => {
-    await Song.sync() // sync --> either CONNECTS or CREATES the table the model represents
+// create an Album table. Use singular here.
+const Album = dbConnection.define("album",{
+    coverUrl: {
+        type: Sequelize.STRING(1000),
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    year: {
+        type: Sequelize.DATE,
+        allowNull: false
+    }
 
-    //another way to create a new row!!
+});
+
+const Artist = dbConnection.define("artist", {
+    name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+    country: {
+        type: Sequelize.STRING()
+    },
+    bio: {
+        type: Sequelize.TEXT
+    }
+});
+
+// belongsTo() is most commonly used association method
+// belongsTo() creates an 'albumId' on your songs table.
+// Song in this is known as the "source"
+// Album in this is known as the "target"
+// the "source" gets a foreign to the "target"
+Song.belongsTo(Album);
+
+// hasMany - create an 'albumId' on your songs table.
+Album.hasMany(Song);
+
+// belongsTo() creates an 'artistId' on your album table.
+// Album is the source, which gets the foreign to the "target"
+// There are many albums to an artist.
+// Therefore, it can also be... Artist.hasMany(Album)
+// Album.belongsTo(Artist);
+
+Album.belongsToMany(Artist, {through: "album_to_artist"});
+Artist.belongsToMany(Album, {through: "album_to_artist"});
+
+const myAsyncFn = async () => {
+    // await Song.sync() // sync --> either CONNECTS or CREATES the table the model represents
+    // That will sync every table that’s connected as a model.
+    await dbConnection.sync({force: true})
+    // force:true creates new table each time. works best with seed.sql
+
+
+    // //another way to create a new row!!
     // const newSong = new Song({
     //     title: "Hello",
     //     duration: 7*60,
@@ -33,23 +85,53 @@ const myAsyncFn = async () => {
 
     // await newSong.save();
 
-    // another way to create a new row!!! this uses .create(), which is new and save combined.
-    const newSong = Song.create({
-        title: "Hotel California",
-        duration: 4.5 * 60,
-        lyrics: "Welcome to the hotel california"
+    // // another way to create a new row!!! this uses .create(), which is new and save combined.
+    // const otherSong = Song.create({
+    //     title: "Hotel California",
+    //     duration: 4.5 * 60,
+    //     lyrics: "Welcome to the hotel california"
+    // });
+
+    const beatlesSong = await Song.create({
+        title: "Yesterday",
+        duration: 200
+    })
+
+    const beatlesAlbumHelp = await Album.create({
+        name: "Help",
+        year: "1967",
     });
 
+    const beatlesAlbumAbby = await Album.create({
+        name: "Abbey Road",
+        year: "1969",
+    });  
+
+    const beatlesArtist = await Artist.create({
+        name: "Beatles",
+    })
+
+    const richieArtist = await Artist.create({
+        name: "Richie"
+    })
+
+    await beatlesAlbumHelp.addArtist(beatlesArtist)
+    await beatlesAlbumAbby.addArtist(beatlesArtist)
+    await beatlesAlbumAbby.addArtist(richieArtist)
+
+    // assign beatles song to beatles album based on id
+    // await beatlesSong.setAlbum(beatlesAlbum.id)
+
     // you can add WHERE clauses
-    const songs = await Song.findAll({
-        where: {
-            title: "Yesterday"
-        }
-    });
+    // const songs = await Song.findAll({
+    //     where: {
+    //         title: "Yesterday"
+    //     }
+    // });
 
     // console.log(songs);
     // console.log(songs[0]);
-    console.log("songs[0].title value is:",songs[0].title);
+    // console.log("songs[0].title value is:",songs[0].title);
     // console.log(songs[1]);
 
 
